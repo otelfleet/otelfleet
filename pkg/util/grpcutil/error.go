10c282699f629e2040e2dd270dd9069be7b7257c
Alexandre Lamarre <alex7285@gmail.com>
2025-12-11 21:01:52 -0500

    feat: grpc util package for error codes

--- pkg/util/grpcutil/error.go
+++ pkg/util/grpcutil/error.go
@@ -12,3 +12,23 @@ func Error(code codes.Code, err error) error {
 func ErrorNotFound(err error) error {
 	return Error(codes.NotFound, err)
 }
+
+func ErrorInternal(err error) error {
+	return Error(codes.Internal, err)
+}
+
+func ErrorInvalid(err error) error {
+	return Error(codes.InvalidArgument, err)
+}
+
+func IsError(code codes.Code, err error) bool {
+	st, ok := status.FromError(err)
+	if !ok {
+		return false
+	}
+	return st.Code() == code
+}
+
+func IsErrorNotFound(err error) bool {
+	return IsError(codes.NotFound, err)
+}
