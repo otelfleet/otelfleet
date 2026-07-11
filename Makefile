408be256aff1f3c78568cbde89dfb085f54df161
Alexandre Lamarre <alex7285@gmail.com>
2026-07-11 09:57:36 -0400

    feat: add version

--- Makefile
+++ Makefile
@@ -1,13 +1,20 @@
 .PHONY: build
 
+VERSION ?= $(shell git describe --tags --always --dirty)
+GITCOMMIT := $(shell git rev-parse --short HEAD)
+
+GOLDFLAGS = -X github.com/otelfleet/otelfleet/pkg/version.Version=${VERSION} \
+	-X github.com/otelfleet/otelfleet/pkg/version.Commit=${GITCOMMIT}
+
+
 build: build-ui build-go
 
 build-ui:
 # 	cd ui && npm run build
 build-agent:
-	go build -o ./bin/agent ./cmd/agent/main.go
+	CGO_ENABLED=0 go build --ldflags="$(GOLDFLAGS)" -o ./bin/agent ./cmd/agent/main.go
 build-go:
-	go build -o ./bin/otelfleet ./cmd/server/main.go
+	CGO_ENABLED=0 go build --ldflags="$(GOLDFLAGS)" -o ./bin/otelfleet ./cmd/server/main.go
 
 build-dev:
 	go build -tags insecure -o ./bin/otelfleet ./cmd/server/main.go
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,6 +11,8 @@ import (
 	"github.com/otelfleet/otelfleet/pkg/config"
 	_ "github.com/otelfleet/otelfleet/pkg/logutil"
 	"github.com/otelfleet/otelfleet/pkg/server"
+	"github.com/otelfleet/otelfleet/pkg/version"
+	"github.com/spf13/cobra"
 )
 
 var (
@@ -20,6 +22,30 @@ var (
 
 func init() {
 	gin.SetMode(gin.ReleaseMode)
+}
+
+func BuildRootCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "otelfleet",
+		Run: func(cmd *cobra.Command, args []string) {
+			logger := slog.Default()
+			logger.With("version", version.FullVersion()).Info("starting otelfleet")
+			srv, err := server.New(config.Config{
+				StoragePath: "./otelfleet.kv",
+			})
+			if err != nil {
+				logger.With("err", err).Error("failed to construct server")
+				os.Exit(1)
+			}
+
+			if err := srv.Run(context.Background()); err != nil {
+				logger.With("err", err).Error("failed to run server")
+				os.Exit(1)
+			}
+		},
+		Version: version.FullVersion(),
+	}
+	return cmd
 }
 
 func loadCerts() *tls.Certificate {
@@ -39,19 +65,12 @@ func loadCerts() *tls.Certificate {
 }
 
 func main() {
-	logger := slog.Default()
-	srv, err := server.New(config.Config{
-		StoragePath: "./otelfleet.kv",
-	})
-	if err != nil {
-		logger.With("err", err).Error("failed to construct server")
-		os.Exit(1)
+	root := BuildRootCommand()
+
+	if err := root.Execute(); err != nil {
+		slog.With("err", err).Error("failed to run otelfleet")
 	}
 
-	if err := srv.Run(context.Background()); err != nil {
-		logger.With("err", err).Error("failed to run server")
-		os.Exit(1)
-	}
 	// logger := slog.Default()
 	// interrupt := make(chan os.Signal, 1)
 	// signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
